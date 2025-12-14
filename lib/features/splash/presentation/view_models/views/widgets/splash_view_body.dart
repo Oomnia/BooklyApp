042a1718eee42d0270f8e4278721ea8a947874db
Omnia Said <omniasaied13201@gmail.com>
2025-12-14 17:45:38 +0200

    Add constants and assets data for the application
    - Introduced a new constants file with primary color definition.
    - Created an assets data class to manage asset paths, starting with the logo.

--- lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
@@ -1,10 +1,24 @@
+import 'package:bookly_app/core/Utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo, height: 90, width: 90),
+        Gap(10),
+        Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        ),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presentation/view_models/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
@@ -12,8 +13,9 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor),
       home: SplashView(),
     );
-    
   }
 }
