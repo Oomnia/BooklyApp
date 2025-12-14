e00ba94c74ea889bc80c6cca540b519f02e9b988
Omnia Said <omniasaied13201@gmail.com>
2025-12-14 19:50:19 +0200

    Refactor SplashViewBody to use StatefulWidget and add animation for text transition

--- lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
@@ -2,8 +2,37 @@ import 'package:bookly_app/core/Utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> textAnimation;
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    textAnimation = Tween<Offset>(
+      begin: Offset(-2, 0),
+      end: Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +42,13 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo, height: 90, width: 90),
         Gap(10),
-        Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20),
+        SlideTransition(
+          position: textAnimation,
+          child: Text(
+            'Read Free Books',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ],
     );
