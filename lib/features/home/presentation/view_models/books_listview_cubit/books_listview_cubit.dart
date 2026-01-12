402454fc034c5fae3218e3a85ba27a4e5cedcaf2
Omnia Said <omniasaied13201@gmail.com>
2026-01-12 13:54:16 +0200

    Implement BooksListviewCubit for fetching featured books and update dependencies

--- lib/features/home/presentation/view_models/books_listview_cubit/books_listview_cubit.dart
+++ lib/features/home/presentation/view_models/books_listview_cubit/books_listview_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/Repos/home_repo.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
 part 'books_listview_state.dart';
 
 class BooksListviewCubit extends Cubit<BooksListviewState> {
-  BooksListviewCubit() : super(BooksListviewInitial());
+  BooksListviewCubit(this.homeRepo) : super(BooksListviewInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(BooksListviewLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure)
+    {
+      emit(BooksListviewFailure(failure.errMessage));
+    },
+    (books) {
+      emit(BooksListviewSuccess(books));
+    });
+  }
 }
